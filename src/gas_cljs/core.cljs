62d523e4c47e92cde43634ff6c72f618114f87c9
meris <meris.bahti@gmail.com>
2025-07-12 14:26:42 +0200

    init

---
+++ src/gas_cljs/core.cljs
@@ -0,0 +1,39 @@
+(ns gas-cljs.core
+  (:require
+   [cljs.math :as math]
+   [reagent.core :as r]
+   [reagent.dom :as d]))
+
+;; -------------------------
+;; Views
+
+(defonce state (r/atom {:users {:me 0 :he 0 :sa 0 :ma 0} :gas-cost 0}))
+
+(defn home-page []
+  (let [total-distance (apply + (-> @state :users vals))]
+    [:div
+     [:ul
+      (for
+       [[person value] (:users @state)] ^{:key person}
+       [:li [:div [:label
+                   person
+                   [:input
+                    {:value value :onChange
+                     (fn [event]
+                       (swap! state (fn [state]   (assoc-in state [:users person] (-> event (.-target) (.-value) (int))))))}]]
+             [:p (math/round (* (:gas-cost @state) (/ value total-distance)))]]])]
+     [:label
+      "gas cost"
+      [:input
+       {:value (:gas-cost @state) :onChange
+        (fn [event]
+          (swap! state (fn [state]  (assoc state :gas-cost (-> event (.-target) (.-value) (int))))))}]]]))
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (d/render [home-page] (.getElementById js/document "app")))
+
+(defn ^:export init! []
+  (mount-root))
